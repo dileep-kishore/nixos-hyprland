bc9aa3d3b7fbcecd9c7787400467436d597ff470
Dileep Kishore <k.dileep1994@gmail.com>
2023-10-21 12:49:58 -0400

    Add: zoxide, gh config and change fzf options

--- home-manager/apps/zsh.nix
+++ home-manager/apps/zsh.nix
@@ -7,10 +7,27 @@
     initExtra = ''
       bindkey '^ ' autosuggest-accept
     '';
+    # antidote.enable = true;
   };
 
   programs.fzf = {
     enable = true;
+    enableBashIntegration = true;
     enableZshIntegration = true;
+    defaultOptions = [
+      "--height 40%"
+      "--reverse"
+      "--preview 'bat --style=numbers --color=always --line-range :500 {}'"
+    ];
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+  };
+
+  programs.gh = {
+    enable = true;
   };
 }
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -22,6 +22,7 @@
   };
 
   home.packages = with pkgs; [
+    bat
     git
     gnome.gedit
     gnome.nautilus
