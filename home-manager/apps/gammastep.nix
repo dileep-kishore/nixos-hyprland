2017fc6c47848fbe2c7fd4cece9f19d0d3ccbdfb
Dileep Kishore <k.dileep1994@gmail.com>
2023-10-23 19:06:27 -0400

    Change: Update dawn and dusk times for gammastep

--- home-manager/apps/gammastep.nix
+++ home-manager/apps/gammastep.nix
@@ -3,8 +3,8 @@
     enable = true;
     latitude = 36.012009;
     longitude = -84.265533;
-    dawnTime = "4:00-6:00";
-    duskTime = "18:00-20:00";
+    dawnTime = "4:00-5:00";
+    duskTime = "18:00-19:00";
     provider = "manual";
     tray = true;
   };
