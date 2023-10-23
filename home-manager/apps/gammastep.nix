50900e10968c3b71c8bb49e2281629d9ccb7c67d
Dileep Kishore <k.dileep1994@gmail.com>
2023-10-23 07:07:37 -0400

    Fix: Dawn and Dusk times for gammastep. Fixes feature/pla-64-update-day-and-night-times-for-gammastep

--- home-manager/apps/gammastep.nix
+++ home-manager/apps/gammastep.nix
@@ -3,6 +3,8 @@
     enable = true;
     latitude = 36.012009;
     longitude = -84.265533;
+    dawnTime = "4:00-6:00";
+    duskTime = "18:00-20:00";
     provider = "manual";
     tray = true;
   };
