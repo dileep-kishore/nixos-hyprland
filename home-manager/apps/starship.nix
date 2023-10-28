034201bce2dd27c924f21333ca3bb25570b2bfe4
Dileep Kishore <k.dileep1994@gmail.com>
2023-10-28 11:24:29 -0400

    Add: Prompt config for starfish prompt

--- home-manager/apps/starship.nix
+++ home-manager/apps/starship.nix
@@ -4,8 +4,146 @@
     enable = true;
     enableBashIntegration = true;
     enableZshIntegration = true;
+    enableTransience = true;
     settings = {
       add_newline = true;
+      character = {
+        format = "$symbol";
+        success_symbol = "[━](bold cyan)[ ](bold cyan)";
+        error_symbol = "[━](bold red)[ ](bold red)";
+        vicmd_symbol = "[━](bold green)[ ](bold green)";
+      };
+      sudo = {
+        format = "[as ](italic)[$symbol]($style)";
+        symbol = " ";
+        disabled = false;
+      };
+      directory = {
+        read_only = "  ";
+        home_symbol = " ";
+        truncation_symbol = "../";
+        truncate_to_repo = true;
+      };
+      status = {
+        disabled = false;
+        symbol = " ";
+        not_executable_symbol = " ";
+      };
+      shlvl = {
+        disabled = false;
+        symbol = " ";
+        threshold = 3;
+      };
+      jobs = {
+        number_threshold = 2;
+        symbol = " ";
+      };
+      cmd_duration = {
+        format = "[took](italic) [$duration]($style) ";
+        min_time = 5000;
+      };
+      memory_usage = {
+        threshold = 80;
+        disabled = false;
+      };
+      # symbols
+      aws = {
+        symbol = "  ";
+      };
+      buf = {
+        symbol = " ";
+      };
+      c = {
+        symbol = " ";
+      };
+      conda = {
+        symbol = " ";
+      };
+      dart = {
+        symbol = " ";
+      };
+      docker_context = {
+        symbol = " ";
+      };
+      elixir = {
+        symbol = " ";
+      };
+      elm = {
+        symbol = " ";
+      };
+      fossil_branch = {
+        symbol = " ";
+      };
+      git_branch = {
+        format = "[on](italic) [$symbol$branch(:$remote_branch)]($style) ";
+        symbol = " ";
+      };
+      golang = {
+        symbol = " ";
+      };
+      guix_shell = {
+        symbol = " ";
+      };
+      haskell = {
+        symbol = " ";
+      };
+      haxe = {
+        symbol = " ";
+      };
+      hg_branch = {
+        symbol = " ";
+      };
+      hostname = {
+        ssh_symbol = " ";
+      };
+      java = {
+        symbol = " ";
+      };
+      julia = {
+        symbol = " ";
+      };
+      lua = {
+        symbol = " ";
+      };
+      memory_usage = {
+        symbol = "󰍛 ";
+      };
+      meson = {
+        symbol = "󰔷 ";
+      };
+      nim = {
+        symbol = "󰆥 ";
+      };
+      nix_shell = {
+        format = "[via](italic) [$symbol$state( \($name\))]($style) ";
+        symbol = " ";
+      };
+      nodejs = {
+        format = "[via](italic) [$symbol($version )]($style)";
+        symbol = "󰎙 ";
+      };
+      package = {
+        symbol = "󰏗 ";
+      };
+      pijul_channel = {
+        symbol = " ";
+      };
+      python = {
+        format = "[via](italic) [$symbol$pyenv_prefix($version )(\($virtualenv\) )]($style)";
+        symbol = " ";
+      };
+      rlang = {
+        symbol = "󰟔 ";
+      };
+      ruby = {
+        symbol = " ";
+      };
+      rust = {
+        symbol = " ";
+      };
+      scala = {
+        symbol = " ";
+      };
     };
   };
 }
