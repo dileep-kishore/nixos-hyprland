4146bc724f79e9613fc354ddf86d6aff35c03295
Dileep Kishore <k.dileep1994@gmail.com>
2023-11-03 13:44:49 -0400

    Add: Mako configuration (catppuccin theme)

--- home-manager/apps/hyprland.nix
+++ home-manager/apps/hyprland.nix
@@ -3,4 +3,6 @@
   home.file.".config/hypr/hyprland.conf".source = ./config/hypr/hyprland.conf;
   home.file.".config/hypr/hyprpaper.conf".source = ./config/hypr/hyprpaper.conf;
   home.file.".config/hypr/mocha.conf".source = ./config/hypr/mocha.conf;
+  # mako
+  home.file.".config/mako/config".source = ./config/mako/config;
 }
