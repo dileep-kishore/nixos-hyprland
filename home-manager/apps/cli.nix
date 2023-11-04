8c813b5861b692671f87e4a92674bb90a02cfff5
Dileep Kishore <k.dileep1994@gmail.com>
2023-11-04 10:38:27 -0400

    Add: Catppuccin theme for fzf

--- home-manager/apps/cli.nix
+++ home-manager/apps/cli.nix
@@ -30,6 +30,9 @@
       "--height 40%"
       "--reverse"
       "--preview 'bat --style=numbers --color=always --line-range :500 {}'"
+      "--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8"
+      "--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc"
+      "--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
     ];
   };
 
