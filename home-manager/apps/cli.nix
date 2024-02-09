851028a8ef4f9be8a20fa55e1e80821fdc2e1272
Dileep Kishore <k.dileep1994@gmail.com>
2024-02-09 18:28:57 -0500

    Fix: Up arrow binding for atuin

--- home-manager/apps/cli.nix
+++ home-manager/apps/cli.nix
@@ -7,10 +7,14 @@
       auto_sync = true;
       sync_frequency = "5m";
       sync_address = "https://api.atuin.sh";
+      filter_mode = "host";
       search_mode = "fuzzy";
+      filter_mode_shell_up_key_binding = "directory";
       search_mode_shell_up_key_binding = "fuzzy";
+      keymap_mode = "vim-insert";
+      show_preview = true;
       style = "compact";
-      invert = true;
+      invert = false;
       inline_height = 20;
     };
   };
