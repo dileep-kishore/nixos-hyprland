2c396aff0820612b14d25c5a72dd23de55066dec
Dileep Kishore <k.dileep1994@gmail.com>
2024-02-26 18:59:25 -0500

    fix: uptime component in tmux status bar

--- home-manager/apps/config/tmux/scripts/uptime_fmt.sh
+++ home-manager/apps/config/tmux/scripts/uptime_fmt.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-uptime | awk '{print $3"D", substr($5, 1, index($5, ":")-1)"H"}'
+# Get the output of the uptime command
+uptime_output=$(uptime)
+
+# Use sed to extract days and hours, handling both cases when days are present and not
+days_hours=$(echo "$uptime_output" | sed -n 's/.*up \+\([0-9]\+\) days, \+\([0-9]\+\):.*/\1D \2H/p')
+
+# If days are not present, just get the hours
+if [ -z "$days_hours" ]; then
+	days_hours=$(echo "$uptime_output" | sed -n 's/.*up \+\([0-9]\+\):.*/0D \1H/p')
+fi
+
+echo "$days_hours"
