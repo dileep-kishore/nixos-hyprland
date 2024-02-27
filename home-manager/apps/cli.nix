20b6cf19cb0b719408b95c96a7bd37489d45fa9c
Dileep Kishore <k.dileep1994@gmail.com>
2024-02-27 01:47:21 -0500

    feat: add rose pine theme for fzf

--- home-manager/apps/cli.nix
+++ home-manager/apps/cli.nix
@@ -34,9 +34,11 @@
       "--height 40%"
       "--reverse"
       "--preview 'bat --style=numbers --color=always --line-range :500 {}'"
-      "--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8"
-      "--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc"
-      "--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+      "--color=fg:#908caa,bg:#191724,hl:#ebbcba"
+      "--color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba"
+      "--color=border:#403d52,header:#31748f,gutter:#191724"
+      "--color=spinner:#f6c177,info:#9ccfd8,separator:#403d52"
+      "--color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
     ];
   };
 
