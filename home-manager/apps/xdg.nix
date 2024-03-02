cd01e35652ed00f496176e1f8ca7427d96fdf7e3
Dileep Kishore <k.dileep1994@gmail.com>
2024-03-02 12:47:55 -0500

    feat: add mimetypes for brave-browser

--- home-manager/apps/xdg.nix
+++ home-manager/apps/xdg.nix
@@ -1,5 +1,4 @@
 {
-
   xdg.enable = true;
 
   xdg.desktopEntries = {
@@ -8,15 +7,19 @@
       genericName = "PDF Viewer";
       exec = "okular %U";
       terminal = false;
-      categories = [ "Application" ];
-      mimeType = [ "application/pdf" ];
+      categories = ["Application"];
+      mimeType = ["application/pdf"];
     };
   };
 
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
-      "application/pdf" = [ "okular.desktop" ];
+      "application/pdf" = ["okular.desktop"];
+      "text/html" = ["brave-browser.desktop"];
+      "application/xhtml+xml" = ["brave-browser.desktop"];
+      "x-scheme-handler/http" = ["brave-browser.desktop"];
+      "x-scheme-handler/https" = ["brave-browser.desktop"];
     };
   };
 }
