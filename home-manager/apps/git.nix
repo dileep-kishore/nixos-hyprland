6850acac431eed71ff41846834fe0c103778ba94
Dileep Kishore <k.dileep1994@gmail.com>
2024-03-28 03:03:34 -0400

    feat: remove side-by-side option for delta

--- home-manager/apps/git.nix
+++ home-manager/apps/git.nix
@@ -2,13 +2,12 @@
   programs.git = {
     enable = true;
     includes = [
-      { path = "~/nixos-hyprland/home-manager/apps/config/gitconfig"; }
+      {path = "~/nixos-hyprland/home-manager/apps/config/gitconfig";}
     ];
     delta = {
       enable = true;
       options = {
-        features = "side-by-side line-numbers decorations";
-        width = 200;
+        features = "line-numbers decorations";
       };
     };
   };
