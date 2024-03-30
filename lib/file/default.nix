819699db0b74e3f13df7242c2ece6b17d4b42e9a
Dileep Kishore <k.dileep1994@gmail.com>
2024-03-30 18:54:58 -0400

    feat: add lib helpers for file

---
+++ lib/file/default.nix
@@ -0,0 +1,23 @@
+{
+  ## Append text to the contents of a file
+  ##
+  ## ```nix
+  ## fileWithText ./some.txt "appended text"
+  ## ```
+  ##
+  #@ Path -> String -> String
+  fileWithText = file: text: ''
+    ${builtins.readFile file}
+    ${text}'';
+
+  ## Prepend text to the contents of a file
+  ##
+  ## ```nix
+  ## fileWithText' ./some.txt "prepended text"
+  ## ```
+  ##
+  #@ Path -> String -> String
+  fileWithText' = file: text: ''
+    ${text}
+    ${builtins.readFile file}'';
+}
