2834874333256e4716e2b1f02fb1283621d506c4
Dileep Kishore <k.dileep1994@gmail.com>
2024-03-30 12:03:23 -0400

    feat: update flake.nix to use snowfall-lib

--- flake.nix
+++ flake.nix
@@ -7,49 +7,59 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     sops-nix.url = "github:Mic92/sops-nix";
     neovim = {
       url = "github:dileep-kishore/neovim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-ld = {
+      url = "github:Mic92/nix-ld";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    sops-nix,
-    ...
-  } @ inputs: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
-    lib = nixpkgs.lib;
-  in {
-    nixosConfigurations = {
-      nixos-xps = lib.nixosSystem {
-        inherit system;
-        specialArgs = inputs;
-        modules = [./nixos/nixos-xps/configuration.nix];
-      };
+  outputs = {snowfall-lib, ...} @ inputs: let
+    lib = snowfall-lib.mkLib {
+      inherit inputs;
+      src = ./.;
 
-      tsuki = lib.nixosSystem {
-        inherit system;
-        specialArgs = inputs;
-        modules = [
-          ./nixos/tsuki/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              extraSpecialArgs = {inherit inputs;};
-              useGlobalPkgs = true;
-              users.dileep = ./home-manager/home.nix;
-            };
-          }
+      snowfall = {
+        meta = {
+          name = "nixos-hyprland";
+          title = "NixOS Hyprland";
+        };
+
+        namespace = "dotfiles";
+      };
+    };
+  in
+    lib.mkFlake {
+      channels-config = {
+        allowUnfree = true;
+        permittedInsecurePackages = [
+          "electron-25.9.0"
         ];
       };
+
+      overlays = with inputs; [];
+
+      systems.modules.nixos = with inputs; [
+        home-manager.nixosModules.home-manager
+        nix-ld.nixosModules.nix-ld
+      ];
+
+      # Hardware modules to optimize for Dell XPS 15 9520
+      systems.hosts.nixos-xps.modules = with inputs; [
+        nixos-hardware.nixosModules.dell-xps-15-9520-nvidia
+      ];
     };
-  };
 }
