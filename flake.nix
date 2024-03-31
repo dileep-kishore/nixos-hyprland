de53c708ea45f4fe4b46f0960664e89e968edc00
Dileep Kishore <k.dileep1994@gmail.com>
2024-03-31 02:14:47 -0400

    feat: basic flake.nix and folder structure for dotfiles v2

--- flake.nix
+++ flake.nix
@@ -3,14 +3,44 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # TODO: Apply fixes to nixos-xps
+    hardware.url = "github:nixos/nixos-hardware";
+    nix-colors.url = "github:misterio77/nix-colors";
+
+    # home-manager
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    sops-nix.url = "github:Mic92/sops-nix";
+
+    # nix-darwin
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # secrets
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-stable.follows = "nixpkgs";
+    };
+
+    # neovim
     neovim = {
       url = "github:dileep-kishore/neovim";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # hyprland
+    hyprland = {
+      url = "github:hyprwm/hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
     };
   };
 
@@ -18,37 +48,72 @@
     self,
     nixpkgs,
     home-manager,
-    sops-nix,
+    nix-darwin,
     ...
   } @ inputs: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
-    lib = nixpkgs.lib;
-  in {
-    nixosConfigurations = {
-      nixos-xps = lib.nixosSystem {
+    inherit (self) outputs;
+    lib = nixpkgs.lib // home-manager.lib // nix-darwin.lib;
+    systems = ["x86_64-linux" "x86_64-darwin"];
+    forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
+    pkgsFor = lib.genAttrs systems (system:
+      import nixpkgs {
         inherit system;
-        specialArgs = inputs;
-        modules = [./nixos/nixos-xps/configuration.nix];
+        config.allowUnfree = true;
+      });
+  in {
+    inherit lib;
+
+    nixosModules = import ./modules/nixos;
+    homeManagerModules = import ./modules/home-manager;
+
+    formatter = forEachSystem (pkgs: pkgs.alejandra);
+
+    # NOTE: home-manager is also imported as a module within nixosConfigurations
+    nixosConfigurations = {
+      # Main desktop
+      tsuki = lib.nixosSystem {
+        modules = [./hosts/tsuki];
+        specialArgs = {inherit inputs outputs;};
       };
 
-      tsuki = lib.nixosSystem {
-        inherit system;
-        specialArgs = inputs;
-        modules = [
-          ./nixos/tsuki/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              extraSpecialArgs = {inherit inputs;};
-              useGlobalPkgs = true;
-              users.dileep = ./home-manager/home.nix;
-            };
-          }
-        ];
+      # Home Laptop
+      nixos-xps = lib.nixosSystem {
+        modules = [./hosts/nixos-xps];
+        specialArgs = {inherit inputs outputs;};
+      };
+    };
+
+    darwinConfigurations = {
+      # Work Laptop
+      Mac124929 = lib.darwinSystem {
+        modules = [./hosts/Mac124929];
+        specialArgs = {inherit inputs outputs;};
+      };
+    };
+
+    homeConfigurations = {
+      # Personal
+      "dileep@tsuki" = lib.homeManagerConfiguration {
+        modules = [./hosts/dileep/tsuki.nix];
+        pkgs = pkgsFor.x86_64-linux;
+        extraSpecialArgs = {inherit inputs outputs;};
+      };
+      "dileep@nixos-xps" = lib.homeManagerConfiguration {
+        modules = [./hosts/dileep/nixos-xps.nix];
+        pkgs = pkgsFor.x86_64-linux;
+        extraSpecialArgs = {inherit inputs outputs;};
+      };
+
+      # Work
+      "g8k@Mac124929" = lib.homeManagerConfiguration {
+        modules = [./hosts/g8k/Mac124929.nix];
+        pkgs = pkgsFor.x86_64-darwin;
+        extraSpecialArgs = {inherit inputs outputs;};
+      };
+      "g8k@lap135849" = lib.homeManagerConfiguration {
+        modules = [./hosts/g8k/lap135849.nix];
+        pkgs = pkgsFor.x86_64-linux;
+        extraSpecialArgs = {inherit inputs outputs;};
       };
     };
   };
