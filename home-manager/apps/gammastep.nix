2c2a4aaf2debd9b3c6eaff05cd77bc6099326e83
Dileep Kishore <k.dileep1994@gmail.com>
2024-04-06 12:34:13 -0400

    fix(gammastep): update brightness setting

--- home-manager/apps/gammastep.nix
+++ home-manager/apps/gammastep.nix
@@ -1,11 +1,21 @@
 {
   services.gammastep = {
     enable = true;
+    provider = "manual";
     latitude = 36.012009;
     longitude = -84.265533;
     dawnTime = "4:00-5:00";
     duskTime = "18:00-19:00";
-    provider = "manual";
+    temperature = {
+      day = 6500;
+      night = 4500;
+    };
+    settings = {
+      general = {
+        brightness-day = 1.0;
+        brightness-night = 1.0;
+      };
+    };
     tray = true;
   };
 }
