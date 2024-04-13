39a0119b26323c3cd74f26467158595f3abd9997
Dileep Kishore <k.dileep1994@gmail.com>
2024-04-13 17:09:11 -0400

    feat(fonts): add font config

---
+++ home/common/core/fonts.nix
@@ -0,0 +1,12 @@
+{pkgs, ...}: {
+  fonts.fontconfig.enable = true;
+  home.packages = with pkgs; [
+    (nerdfonts.override {
+      fonts = ["FiraCode" "CascadiaCode" "FantasqueSansMono"];
+    })
+    font-awesome
+    noto-fonts
+    noto-fonts-emoji
+    recursive
+  ];
+}
