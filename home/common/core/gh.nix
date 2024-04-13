c98c2121955c77800794f28cbe0e15d787235e08
Dileep Kishore <k.dileep1994@gmail.com>
2024-04-13 13:03:19 -0400

    feat(gh): add github cli config

---
+++ home/common/core/gh.nix
@@ -0,0 +1,14 @@
+# https://cli.github.com/
+{pkgs, ...}: {
+  programs.gh = {
+    enable = true;
+    extensions = with pkgs; [gh-markdown-preview];
+    settings = {
+      git_protocol = "ssh";
+      prompt = "enabled";
+    };
+  };
+  programs.gh-dash = {
+    enable = true;
+  };
+}
