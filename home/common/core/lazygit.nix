10cb411c182231045e824c78708adecf336af36e
Dileep Kishore <k.dileep1994@gmail.com>
2024-04-13 12:45:54 -0400

    feat(lazygit): add lazygit config

---
+++ home/common/core/lazygit.nix
@@ -0,0 +1,12 @@
+# https://github.com/jesseduffield/lazygit
+{
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      git.paging = {
+        colorArg = "always";
+        pager = "delta --dark --paging=never";
+      };
+    };
+  };
+}
