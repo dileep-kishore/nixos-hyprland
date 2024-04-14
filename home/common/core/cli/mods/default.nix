c8c4a6eeb4f412b56871ebe83f5198bba2c91d94
Dileep Kishore <k.dileep1994@gmail.com>
2024-04-14 12:27:05 -0400

    fix(deps): add mods and waybar packages to their modules

--- home/common/core/cli/mods/default.nix
+++ home/common/core/cli/mods/default.nix
@@ -1,5 +1,9 @@
 # https://github.com/charmbracelet/mods
-{
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    mods
+  ];
+
   # copy config
   home.file.".config/mods/mods.yml".source = ./mods.yml;
 }
--- home/common/optional/desktops/waybar/default.nix
+++ home/common/optional/desktops/waybar/default.nix
@@ -1,6 +1,10 @@
 # https://github.com/Alexays/Waybar
 # TODO: Use HM to set up the waybar config
-{
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    waybar
+  ];
+
   home.file.".config/waybar" = {
     recursive = true;
     source = ./config;
