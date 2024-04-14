b12df9b389bfcec492b795d8e6c5fe6b26005102
Dileep Kishore <k.dileep1994@gmail.com>
2024-04-14 10:31:27 -0400

    feat(hyprland): add support for different monitor configuratations

--- home/common/optional/desktops/hyprland/default.nix
+++ home/common/optional/desktops/hyprland/default.nix
@@ -1,8 +1,12 @@
 # https://github.com/hyprwm/Hyprland
 # TODO: Use HM to set up the hyprland config
-{
+{config, ...}: {
   home.file.".config/hypr" = {
     recursive = true;
     source = ./hypr;
+  };
+  # Include monitors_x.conf depending on the hostname in nix
+  home.file.".config/hypr/monitors.conf" = {
+    source = ./monitors_${config.networking.hostName}.conf;
   };
 }
