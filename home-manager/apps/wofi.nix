2e0bd3d89c2a885f9675b334d795e832abffdba9
Dileep Kishore <k.dileep1994@gmail.com>
2024-04-14 09:12:43 -0400

    feat(wofi): add wofi config

--- home-manager/apps/wofi.nix
+++
@@ -1,9 +0,0 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
-  home.file.".config/wofi/config".source = ./config/wofi/config;
-  home.file.".config/wofi/style.css".source = ./config/wofi/style.css;
-}
---
+++ home/common/optional/apps/wofi/default.nix
@@ -0,0 +1,16 @@
+{
+  programs.wofi = {
+    enable = true;
+    settings = {
+      show = "drun";
+      width = 750;
+      height = 400;
+      always_parse_args = true;
+      show_all = false;
+      print_command = true;
+      insensitive = true;
+      prompt = "Hmm, what do you want to run?";
+    };
+    style = builtins.readFile ./style.css;
+  };
+}
