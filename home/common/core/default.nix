2894a17b6eaa6668909e01a9a8a6617bf30d2dab
Dileep Kishore <k.dileep1994@gmail.com>
2024-04-14 13:32:07 -0400

    feat(home-manager): add more default home-manager options

--- home/common/core/default.nix
+++ home/common/core/default.nix
@@ -1,17 +1,34 @@
 {
-  config,
+  outputs,
   lib,
   pkgs,
-  outputs,
   ...
 }: {
-  imports =
-    [
-      ./apps
-      ./cli
-      ./code
-      ./fonts.nix
-      ./shells
-    ]
-    ++ (builtins.attrValues outputs.homeManagerModules);
+  imports = [
+    ./apps
+    ./cli
+    ./code
+    ./fonts.nix
+    ./shells
+  ];
+
+  nixpkgs = {
+    overlays = builtins.attrValues outputs.overlays;
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+  nix = {
+    package = lib.mkDefault pkgs.nix;
+    settings = {
+      experimental-features = ["nix-command" "flakes" "repl-flake"];
+      warn-dirty = false;
+    };
+  };
+
+  programs.home-manager.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
 }
