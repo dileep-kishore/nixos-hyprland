1e8e66e839fc18e0ee17598fa82d9ce38439e60c
Dileep Kishore <k.dileep1994@gmail.com>
2024-04-14 09:58:30 -0400

    feat(zsh): add zsh config

--- home/common/core/shells/zsh.nix
+++ home/common/core/shells/zsh.nix
@@ -1,43 +1,4 @@
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
-  home.shellAliases = {
-    # rm alias
-    rm = "rm -i";
-    # clear
-    clear = "clear -x";
-    cls = "clear";
-    # vim alias
-    v = "nvim";
-    # yazi alias
-    y = "yazi";
-    # tmux
-    tnew = "tmux new -s";
-    ta = "tmux attach -t";
-    tdetach = "tmux detach";
-    tkill = "tmux kill-session -t";
-    tlist = "tmux ls";
-    # bat
-    bat = "bat --theme='Catppuccin-mocha'";
-    # grep
-    grep = "grep --color=auto";
-    # python
-    vimipython = "ipython --TerminalInteractiveShell.editing_mode=vi";
-    # conda
-    exportforge = "export PATH=$HOME/miniforge3/bin:$PATH";
-    # network
-    myip = "curl http://ipecho.net/plain; echo";
-    # ncdu
-    df = "df -ahiT --total";
-    du = "ncdu --color dark -rr -x --exclude .git --exclude node_modules";
-    # memory
-    free = "free -mt";
-    ps = "ps auxf";
-    psgrep = "ps aux | grep -v grep | grep -i -e VSZ -e";
-  };
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -45,6 +6,7 @@
     syntaxHighlighting.enable = true;
     autocd = true;
     history.extended = true;
+    # NOTE: You can use these as `cd ~dots` or `cd ~docs` etc.
     dirHashes = {
       dots = "$HOME/nixos-hyprland";
       docs = "$HOME/Documents";
@@ -57,7 +19,6 @@
       bindkey '^ ' autosuggest-accept
       export OPENAI_API_KEY="$(cat ~/.secrets/openai_api_key.txt)"
       export DIRENV_LOG_FORMAT=""
-      eval "$(gh copilot alias -- zsh)"
     '';
     sessionVariables = {
       EDITOR = "nvim";
@@ -65,7 +26,6 @@
       USE_EDITOR = "$EDITOR";
       VISUAL = "$EDITOR";
       BROWSER = "brave";
-      BAT_THEME = "Catppuccin-mocha";
     };
     # profileExtra = { };
     # shellAliases = { };
