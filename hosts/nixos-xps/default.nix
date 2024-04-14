f3f6e3c8354284dde51cf9370d7fa5b0bd8acdf3
Dileep Kishore <k.dileep1994@gmail.com>
2024-04-14 17:44:42 -0400

    feat(hosts): Move nixos-xps configuration to modular structure

--- hosts/nixos-xps/default.nix
+++ hosts/nixos-xps/default.nix
@@ -1,1 +1,76 @@
-{}
+{
+  pkgs,
+  config,
+  inputs,
+  outputs,
+  ...
+}: {
+  imports =
+    [
+      # Hardware config
+      inputs.hardware.nixosModules.dell-xps-15-9520-nvidia
+      ./hardware-configuration.nix
+
+      # Common config
+      ../common/core
+
+      # Optional configs
+      ../common/optional/hyprland.nix
+      ../common/optional/wacom.nix
+
+      # User config
+      ../common/users/dileep
+    ]
+    ++ (builtins.attrValues outputs.nixosModules);
+
+  # Bootloader.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.configurationLimit = 15;
+  boot.loader.systemd-boot.configurationLimit = 15;
+  boot.supportedFilesystems = ["ntfs"];
+
+  networking.hostName = "nixos-xps"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.xkbOptions = "ctrl:nocaps";
+  console.useXkbConfig = true;
+  hardware.bluetooth.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    enable = true;
+    xkb.options = "ctrl:nocaps";
+    xkb.layout = "us";
+    xkb.variant = "";
+    displayManager.gdm.enable = true;
+  };
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    powerManagement.finegrained = false;
+    open = true;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    prime = {
+      sync.enable = true;
+      intelBusId = "PCI:0:02:0";
+      nvidiaBusId = "PCI:1:00:0";
+    };
+  };
+
+  system.stateVersion = "23.11";
+}
