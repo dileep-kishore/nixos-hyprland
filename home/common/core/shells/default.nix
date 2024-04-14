9b86266ecfee1ebd52dd873de7a3447ff09cd1d9
Dileep Kishore <k.dileep1994@gmail.com>
2024-04-14 09:59:28 -0400

    feat(shells): add common shell config

---
+++ home/common/core/shells/default.nix
@@ -0,0 +1,32 @@
+{
+  imports = [
+    ./bash.nix
+    ./zsh.nix
+  ];
+  home.shellAliases = {
+    # rm alias
+    rm = "rm -i";
+    # clear
+    clear = "clear -x";
+    cls = "clear";
+    # vim alias
+    v = "nvim";
+    # grep
+    grep = "grep --color=auto";
+    # python
+    vimipython = "ipython --TerminalInteractiveShell.editing_mode=vi";
+    # conda
+    exportforge = "export PATH=$HOME/miniforge3/bin:$PATH";
+    # network
+    myip = "curl http://ipecho.net/plain; echo";
+    # df
+    df = "df -ahiT --total";
+    # ncdu
+    du = "ncdu --color dark -rr -x --exclude .git --exclude node_modules";
+    # memory
+    free = "free -mt";
+    # ps
+    ps = "ps auxf";
+    psgrep = "ps aux | grep -v grep | grep -i -e VSZ -e";
+  };
+}
