8a12662873de90da692cf7caa9ab890fb28df261
Dileep Kishore <k.dileep1994@gmail.com>
2024-04-18 15:50:49 -0400

    feat(pkgs): add setup to support custom pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,1 +1,6 @@
-{}
+# You can build these directly using 'nix build .#example'
+{pkgs ? import <nixpkgs> {}}: rec {
+  #################### Packages with external source ####################
+
+  # example = pkgs.callPackage ./example { };
+}
