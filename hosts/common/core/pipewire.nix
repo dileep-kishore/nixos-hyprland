8c8ed5b2346a9829f027f613ea8650cdab1ce68e
Dileep Kishore <k.dileep1994@gmail.com>
2024-04-20 13:00:48 -0400

    fix(hyprland): screensharing issue

--- hosts/common/core/pipewire.nix
+++ hosts/common/core/pipewire.nix
@@ -5,6 +5,7 @@
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
+    wireplumber.enable = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     jack.enable = true;
