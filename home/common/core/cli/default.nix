05c9f48cb849753ddb121ef71479dabc69a1cf35
Dileep Kishore <k.dileep1994@gmail.com>
2024-04-21 15:29:08 -0400

    feat(jujutsu): add jujutsu to cli apps

--- home/common/core/cli/default.nix
+++ home/common/core/cli/default.nix
@@ -12,6 +12,7 @@
     ./fzf.nix
     ./gh.nix
     ./git.nix
+    ./jujutsu.nix
     ./lazygit.nix
     ./mods
     ./ripgrep.nix
---
+++ home/common/core/cli/jujutsu.nix
@@ -0,0 +1,5 @@
+{
+  programs.jujutsu = {
+    enable = true;
+  };
+}
