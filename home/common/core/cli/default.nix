04aa506a295cf53e8abd1f89ec744afe473e3eca
Dileep Kishore <k.dileep1994@gmail.com>
2024-10-10 02:33:54 -0400

    feat(tmux): update git status based on tokyonight theme

--- home/common/core/cli/default.nix
+++ home/common/core/cli/default.nix
@@ -26,6 +26,7 @@
 
   home.packages = with pkgs; [
     inputs.neovim.packages.${system}.default
+    bc
     coreutils-full
     entr
     fd
--- home/common/core/cli/tmux/scripts/git_status.sh
+++ home/common/core/cli/tmux/scripts/git_status.sh
@@ -27,15 +27,100 @@ thm_surface_0="#313244"
 thm_mantle="#181825"
 thm_crust="#11111b"
 
-cd "$1" || exit
+# cd "$1" || exit
+# BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+# STATUS=$(git status --porcelain 2>/dev/null | grep -c -E "^(M| M)")
+# if test "$BRANCH" != ""; then
+# 	if test "$STATUS" = "0"; then
+# 		echo "#[bg=$thm_peach,fg=$thm_base]#[fg=$thm_base,bg=$thm_peach,italics]  $BRANCH #[bg=$thm_base,fg=$thm_peach]"
+# 	else
+# 		echo "#[bg=$thm_red,fg=$thm_base]#[fg=$thm_base,bg=$thm_red,italics]  $BRANCH #[bg=$thm_base,fg=$thm_red]"
+# 	fi
+# else
+# 	echo ""
+# fi
+
+RESET="#[fg=${thm_text},bg=${thm_base},nobold,noitalics,nounderscore,nodim]"
 BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-STATUS=$(git status --porcelain 2>/dev/null | grep -c -E "^(M| M)")
-if test "$BRANCH" != ""; then
-	if test "$STATUS" = "0"; then
-		echo "#[bg=$thm_peach,fg=$thm_base]#[fg=$thm_base,bg=$thm_peach,italics]  $BRANCH #[bg=$thm_base,fg=$thm_peach]"
+STATUS=$(git status --porcelain 2>/dev/null | grep -cE "^(M| M)")
+
+SYNC_MODE=0
+NEED_PUSH=0
+
+if [[ ${#BRANCH} -gt 25 ]]; then
+	BRANCH="${BRANCH:0:25}…"
+fi
+
+STATUS_CHANGED=""
+STATUS_INSERTIONS=""
+STATUS_DELETIONS=""
+STATUS_UNTRACKED=""
+
+if [[ $STATUS -ne 0 ]]; then
+	DIFF_COUNTS=($(git diff --numstat 2>/dev/null | awk 'NF==3 {changed+=1; ins+=$1; del+=$2} END {printf("%d %d %d", changed, ins, del)}'))
+	CHANGED_COUNT=${DIFF_COUNTS[0]}
+	INSERTIONS_COUNT=${DIFF_COUNTS[1]}
+	DELETIONS_COUNT=${DIFF_COUNTS[2]}
+
+	SYNC_MODE=1
+fi
+
+UNTRACKED_COUNT="$(git ls-files --other --directory --exclude-standard | wc -l | bc)"
+
+if [[ $CHANGED_COUNT -gt 0 ]]; then
+	STATUS_CHANGED="${RESET}#[fg=${thm_yellow},bg=${thm_base},bold] ${CHANGED_COUNT} "
+fi
+
+if [[ $INSERTIONS_COUNT -gt 0 ]]; then
+	STATUS_INSERTIONS="${RESET}#[fg=${thm_green},bg=${thm_base},bold] ${INSERTIONS_COUNT} "
+fi
+
+if [[ $DELETIONS_COUNT -gt 0 ]]; then
+	STATUS_DELETIONS="${RESET}#[fg=${thm_red},bg=${thm_base},bold] ${DELETIONS_COUNT} "
+fi
+
+if [[ $UNTRACKED_COUNT -gt 0 ]]; then
+	STATUS_UNTRACKED="${RESET}#[fg=${thm_crust},bg=${thm_base},bold] ${UNTRACKED_COUNT} "
+fi
+
+# Determine repository sync status
+if [[ $SYNC_MODE -eq 0 ]]; then
+	NEED_PUSH=$(git log @{push}.. | wc -l | bc)
+	if [[ $NEED_PUSH -gt 0 ]]; then
+		SYNC_MODE=2
 	else
-		echo "#[bg=$thm_red,fg=$thm_base]#[fg=$thm_base,bg=$thm_red,italics]  $BRANCH #[bg=$thm_base,fg=$thm_red]"
+		LAST_FETCH=$(stat -c %Y .git/FETCH_HEAD | bc)
+		NOW=$(date +%s | bc)
+
+		# if 5 minutes have passed since the last fetch
+		if [[ $((NOW - LAST_FETCH)) -gt 300 ]]; then
+			git fetch --atomic origin --negotiation-tip=HEAD
+		fi
+
+		# Check if the remote branch is ahead of the local branch
+		REMOTE_DIFF="$(git diff --numstat "${BRANCH}" "origin/${BRANCH}" 2>/dev/null)"
+		if [[ -n $REMOTE_DIFF ]]; then
+			SYNC_MODE=3
+		fi
 	fi
-else
-	echo ""
+fi
+
+# Set the status indicator based on the sync mode
+case "$SYNC_MODE" in
+1)
+	REMOTE_STATUS="$RESET#[bg=${thm_base},fg=${thm_red},bold]▒ 󱓎"
+	;;
+2)
+	REMOTE_STATUS="$RESET#[bg=${thm_base},fg=${thm_red},bold]▒ 󰛃"
+	;;
+3)
+	REMOTE_STATUS="$RESET#[bg=${thm_base},fg=${thm_maroon},bold]▒ 󰛀"
+	;;
+*)
+	REMOTE_STATUS="$RESET#[bg=${thm_base},fg=${thm_green},bold]▒ "
+	;;
+esac
+
+if [[ -n $BRANCH ]]; then
+	echo "$REMOTE_STATUS $RESET$BRANCH $STATUS_CHANGED$STATUS_INSERTIONS$STATUS_DELETIONS$STATUS_UNTRACKED"
 fi
