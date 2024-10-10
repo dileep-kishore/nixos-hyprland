a072df105fdd9045893187ea55618c640270612d
Dileep Kishore <k.dileep1994@gmail.com>
2024-10-09 22:25:03 -0400

    chore: disable nameservers and extraHosts (temp)

--- hosts/common/core/networking.nix
+++ hosts/common/core/networking.nix
@@ -1,14 +1,14 @@
 {
   networking = {
     networkmanager.enable = true;
-    nameservers = ["208.67.222.123" "208.67.220.123"];
-    extraHosts = let
-      hostsPath = "https://raw.githubusercontent.com/StevenBlack/hosts/1a8e75f1cb6cd3b26ff028df83121f3d7b67b04c/alternates/porn/hosts";
-      hostsFile = builtins.fetchurl {
-        url = hostsPath;
-        sha256 = "sha256:15vvv67piwmvgibjih8ifgs0q8z0pwsad5adyppijinq4fihwf4n";
-      };
-    in
-      builtins.readFile "${hostsFile}";
+    # nameservers = ["208.67.222.123" "208.67.220.123"];
+    # extraHosts = let
+    #   hostsPath = "https://raw.githubusercontent.com/StevenBlack/hosts/1a8e75f1cb6cd3b26ff028df83121f3d7b67b04c/alternates/porn/hosts";
+    #   hostsFile = builtins.fetchurl {
+    #     url = hostsPath;
+    #     sha256 = "sha256:15vvv67piwmvgibjih8ifgs0q8z0pwsad5adyppijinq4fihwf4n";
+    #   };
+    # in
+    #   builtins.readFile "${hostsFile}";
   };
 }
