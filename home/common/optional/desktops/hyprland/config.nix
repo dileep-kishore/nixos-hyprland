b1c6141111b432ff0e5f6e91c2d0e011098f66ff
Dileep Kishore <k.dileep1994@gmail.com>
2024-10-13 21:07:09 -0400

    feat(hyprland): move full config to nix

---
+++ home/common/optional/desktops/hyprland/config.nix
@@ -0,0 +1,175 @@
+{
+  wayland.windowManager.hyprland = {
+    enable = true;
+    systemd.enable = true;
+    xwayland.enable = true;
+    sourceFirst = true;
+    settings = {
+      source = [
+        "~/.config/hypr/mocha.conf" # Theme
+        "~/.config/hypr/monitors.conf"
+      ];
+      exec-once = [
+        # TODO: Might need to remove if we start these services from home-manager
+        "waybar & hyprpaper"
+        "hyprctl setcursor Bibata-Original-Ice 24"
+        # TODO: Include hypridle and hyprlock
+        "1password"
+      ];
+      env = [
+        "XCURSOR_SIZE,24"
+        "WLR_DRM_DEVICES,/dev/dri/card0:/dev/dri/card1"
+      ];
+      input = {
+        kb_layout = "us";
+        kb_options = "ctrl:nocaps";
+        follow_mouse = 1;
+        touchpad = {
+          natural_scroll = false;
+        };
+        sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
+      };
+      general = {
+        gaps_in = 5;
+        gaps_out = 5;
+        border_size = 4;
+        "col.active_border" = "$mauve";
+        "col.inactive_border" = "$surface0";
+        layout = "dwindle";
+      };
+      decoration = {
+        rounding = 10;
+        active_opacity = 1.0;
+        inactive_opacity = 0.95;
+        dim_inactive = true;
+        dim_strength = 0.1;
+        blur = {
+          enabled = true;
+          size = 8;
+          passes = 2;
+          popups = true;
+        };
+        drop_shadow = true;
+        shadow_range = 4;
+        shadow_render_power = 3;
+        "col.shadow" = "$base";
+      };
+      binds = {movefocus_cycles_fullscreen = false;};
+      animations = {
+        enabled = true;
+        bezier = [
+          "wind, 0.05, 0.9, 0.1, 1.05"
+          "winIn, 0.1, 1.1, 0.1, 1.1"
+          "winOut, 0.3, -0.3, 0, 1"
+          "liner, 1, 1, 1, 1"
+        ];
+        animation = [
+          "windows, 1, 6, wind, slide"
+          "windowsIn, 1, 6, winIn, slide"
+          "windowsOut, 1, 5, winOut, slide"
+          "windowsMove, 1, 5, wind, slide"
+          "border, 1, 1, liner"
+          "borderangle, 1, 200, liner, loop"
+          "fade, 1, 10, default"
+          "workspaces, 1, 5, wind"
+        ];
+      };
+      dwindle = {
+        pseudotile = true;
+        preserve_split = true;
+        force_split = 2;
+        default_split_ratio = 1.2;
+      };
+      master = {new_is_master = true;};
+      gestures = {workspace_swipe = false;};
+      misc = {
+        disable_hyprland_logo = true;
+        mouse_move_enables_dpms = true;
+        key_press_enables_dpms = true;
+      };
+      # Window rules
+      windowrulev2 = [
+        # Ulauncher window rules
+        "noborder,title:^(.*ulauncher.*)$"
+        "noborder,title:^(.*Ulauncher.*)$"
+        "noshadow,title:^(.*ulauncher.*)$"
+        "noshadow,title:^(.*Ulauncher.*)$"
+        "dimaround,title:^(.*ulauncher.*)$"
+        "dimaround,title:^(.*Ulauncher.*)$"
+        # Floating kitty terminal window rules
+        "float,class:^(kitty-float)$"
+        "move 10% 59%,class:^(kitty-float)$"
+        "size 80% 40%,class:^(kitty-float)$"
+      ];
+      # Keybindings
+      "$mainMod" = "SUPER";
+      "$subMod" = "SUPER_SHIFT";
+      "$tetMod" = "SUPER_CTRL";
+      bind =
+        [
+          "$mainMod, return, exec, kitty"
+          "$subMod, return, exec, hdrop kitty --class 'kitty-float'"
+          "$subMod, Q, killactive,"
+          "$subMod, E, exit,"
+          "$subMod, M, exec, pkill waybar && waybar"
+          # TODO: Keybind for hyprlock
+          "$mainMod, E, exec, nautilus"
+          "$mainMod, R, exec, ulauncher"
+          "$tetMod, D, pseudo," # dwindle
+          "$tetMod, J, togglesplit," # dwindle
+          "$tetMod, S, exec, ~/.config/hypr/scripts/screenshot.sh region"
+          "$tetMod, P, exec, ~/.config/hypr/scripts/screenshot.sh activemonitor"
+          "$tetMod, W, exec, ~/.config/hypr/scripts/screenshot.sh activewindow"
+          # Move focus with mainMod + arrow keys
+          "$mainMod, h, movefocus, l"
+          "$mainMod, l, movefocus, r"
+          "$mainMod, k, movefocus, u"
+          "$mainMod, j, movefocus, d"
+          # Move to next/previous workspace with subMod + h/l
+          "$subMod, H, workspace, m-1"
+          "$subMod, L, workspace, m+1"
+          # Resize windows with subMod + h,j,k,l
+          "$subMod, left, resizeactive, -10 0"
+          "$subMod, down, resizeactive, 0 10"
+          "$subMod, up, resizeactive, 0 -10"
+          "$subMod, right, resizeactive, 10 0"
+          # Scroll through existing workspaces with mainMod + scroll
+          "$mainMod, mouse_down, workspace, m-1"
+          "$mainMod, mouse_up, workspace, m+1"
+          # Layout toggles
+          "$mainMod, t, layoutmsg, togglesplit"
+          "$mainMod, s, layoutmsg, swapsplit"
+          "$mainMod, w, swapnext"
+          "$mainMod, m, fullscreen, 1"
+          "$subMod, m, fullscreen, 0"
+          "$mainMod, f, togglefloating"
+          # Move current workspace to another monitor
+          # NOTE: This will only work on tsuki (assuming vert=1, top=2 and bottom=3)
+          "$tetMod, 1, movecurrentworkspacetomonitor, HDMI-A-1"
+          "$tetMod, 2, movecurrentworkspacetomonitor, DP-1"
+          "$tetMod, 3, movecurrentworkspacetomonitor, DP-2"
+        ]
+        ++ (
+          # Switch workspaces with mainMod + [0-9]
+          # Move active window to a workspace with subMod + [0-9]
+          builtins.concatLists (builtins.genList (
+              i: let
+                ws = i + 1;
+              in [
+                "$mainMod, code:1${toString i}, workspace, ${toString ws}"
+                "$subMod, code:1${toString i}, movetoworkspace, ${toString ws}"
+              ]
+            )
+            9)
+        );
+      bindm = [
+        # Move/resize windows with mainMod + LMB/RMB and dragging
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+      ];
+    };
+    # plugins = [];
+    # extraConfig = "";
+  };
+  # TODO: programs.hyprlock, services.hypridle, services.hyprpaper, progra
+}
