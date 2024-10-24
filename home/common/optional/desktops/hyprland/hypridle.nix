43271203d31bc88da8b55d64d41344f6371d37d9
Dileep Kishore <k.dileep1994@gmail.com>
2024-10-23 21:18:22 -0400

    fix(hyprland): disable lock on system sleep

--- home/common/optional/desktops/hyprland/hypridle.nix
+++ home/common/optional/desktops/hyprland/hypridle.nix
@@ -4,7 +4,7 @@
     settings = {
       general = {
         lock_cmd = "pidof hyprlock || hyprlock";
-        before_sleep_cmd = "loginctl lock-session";
+        # before_sleep_cmd = "loginctl lock-session";
         after_sleep_cmd = "hyprctl dispatch dpms on";
       };
       listener = [
