9b2db60f7dd5a0fa5e301fcff8d41cc94cf8c60d
Dileep Kishore <k.dileep1994@gmail.com>
2025-04-03 01:41:25 -0400

    feat(aider): add playwright deps

--- home/common/core/cli/aider/default.nix
+++ home/common/core/cli/aider/default.nix
@@ -1,6 +1,10 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     aider-chat
+    python312Packages.playwright
+    python312Packages.greenlet
+    python312Packages.pyee
+    python312Packages.typing-extensions
   ];
   home.file.".aider.conf.yml".source = ./.aider.conf.yml;
 }
