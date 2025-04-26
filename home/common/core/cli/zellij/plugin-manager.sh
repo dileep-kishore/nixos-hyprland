b4d9961af8de5a360f0e69877a1953abc187ae7b
Dileep Kishore <k.dileep1994@gmail.com>
2025-04-26 16:17:11 -0400

    feat(zellij): add zellij-choose-tree plugin

--- home/common/core/cli/zellij/plugin-manager.sh
+++ home/common/core/cli/zellij/plugin-manager.sh
@@ -8,3 +8,4 @@ aria2c --allow-overwrite=true -d "$PLUGIN_DIR" -o zjstatus.wasm https://github.c
 aria2c --allow-overwrite=true -d "$PLUGIN_DIR" -o zjframes.wasm https://github.com/dj95/zjstatus/releases/latest/download/zjframes.wasm
 aria2c --allow-overwrite=true -d "$PLUGIN_DIR" -o zellij_forgot.wasm https://github.com/karimould/zellij-forgot/releases/latest/download/zellij_forgot.wasm
 aria2c --allow-overwrite=true -d "$PLUGIN_DIR" -o zellij-autolock.wasm https://github.com/fresh2dev/zellij-autolock/releases/latest/download/zellij-autolock.wasm
+aria2c --allow-overwrite=true -d "$PLUGIN_DIR" -o zellij-choose-tree.wasm https://github.com/laperlej/zellij-choose-tree/releases/latest/download/zellij-choose-tree.wasm
