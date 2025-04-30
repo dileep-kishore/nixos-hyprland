1d39c59754b15797d8e14ea23c7fb15019c6686a
dileep-kishore <k.dileep1994@gmail.com>
2025-04-30 17:02:05 -0400

    feat(niri): export PATH in walker script

--- home/common/optional/desktops/niri/niri/scripts/walker.sh
+++ home/common/optional/desktops/niri/niri/scripts/walker.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env zsh
 
 export ANTHROPIC_API_KEY=$(cat ~/.secrets/anthropic_api_key.txt)
+export PATH="$PATH:$HOME/.local/bin:/var/lib/flatpak/exports/bin:$HOME/.nix-profile/bin"
+
 walker
