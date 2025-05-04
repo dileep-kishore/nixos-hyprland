bd34e4e593e2dfd939f7022bba36ad9cea8984dd
Dileep Kishore <k.dileep1994@gmail.com>
2025-05-04 06:07:35 -0400

    chore: update flake

--- home/common/optional/desktops/services/mako/default.nix
+++ home/common/optional/desktops/services/mako/default.nix
@@ -2,11 +2,21 @@
 {
   services.mako = {
     enable = true;
-    anchor = "top-right";
-    borderRadius = 2;
-    borderSize = 2;
-    font = "Maple Mono";
-    icons = true;
-    extraConfig = builtins.readFile ./config;
+    settings = {
+      anchor = "top-right";
+      borderRadius = "2";
+      borderSize = "2";
+      font = "Maple Mono";
+      icons = "true";
+      backgroundColor = "#1e1e2e";
+      textColor = "#cdd6f4";
+      borderColor = "#cba6f7";
+      progressColor = "#313244";
+    };
+    criterias = {
+      "urgency=high" = {
+        borderColor = "#fab387";
+      };
+    };
   };
 }
