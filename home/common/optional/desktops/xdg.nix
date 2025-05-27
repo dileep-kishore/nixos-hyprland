9a65379270fd56aece349e6c3b0c28009ec8c9ac
Dileep Kishore <k.dileep1994@gmail.com>
2025-05-27 19:12:58 -0400

    feat(xdg): fix zen desktop file name

--- home/common/optional/desktops/xdg.nix
+++ home/common/optional/desktops/xdg.nix
@@ -17,16 +17,16 @@
     defaultApplications = {
       "application/pdf" = ["okular.desktop"];
       "x-scheme-handler/tana" = ["tana.desktop"];
-      "x-scheme-handler/http" = ["zen-browser.desktop"];
-      "x-scheme-handler/https" = ["zen-browser.desktop"];
-      "x-scheme-handler/chrome" = ["zen-browser.desktop"];
-      "text/html" = ["zen-browser.desktop"];
-      "application/x-extension-htm" = ["zen-browser.desktop"];
-      "application/x-extension-html" = ["zen-browser.desktop"];
-      "application/x-extension-shtml" = ["zen-browser.desktop"];
-      "application/xhtml+xml" = ["zen-browser.desktop"];
-      "application/x-extension-xhtml" = ["zen-browser.desktop"];
-      "application/x-extension-xht" = ["zen-browser.desktop"];
+      "x-scheme-handler/http" = ["zen.desktop"];
+      "x-scheme-handler/https" = ["zen.desktop"];
+      "x-scheme-handler/chrome" = ["zen.desktop"];
+      "text/html" = ["zen.desktop"];
+      "application/x-extension-htm" = ["zen.desktop"];
+      "application/x-extension-html" = ["zen.desktop"];
+      "application/x-extension-shtml" = ["zen.desktop"];
+      "application/xhtml+xml" = ["zen.desktop"];
+      "application/x-extension-xhtml" = ["zen.desktop"];
+      "application/x-extension-xht" = ["zen.desktop"];
     };
   };
 }
