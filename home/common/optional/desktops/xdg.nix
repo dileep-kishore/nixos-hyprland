972da12b9e9722f5244b7bd0a95c388906692317
Dileep Kishore <k.dileep1994@gmail.com>
2025-06-14 20:27:48 -0400

    feat(xdg): make vivaldi the default browser

--- home/common/optional/desktops/xdg.nix
+++ home/common/optional/desktops/xdg.nix
@@ -17,16 +17,16 @@
     defaultApplications = {
       "application/pdf" = ["okular.desktop"];
       "x-scheme-handler/tana" = ["tana.desktop"];
-      "x-scheme-handler/http" = ["zen.desktop"];
-      "x-scheme-handler/https" = ["zen.desktop"];
-      "x-scheme-handler/chrome" = ["zen.desktop"];
-      "text/html" = ["zen.desktop"];
-      "application/x-extension-htm" = ["zen.desktop"];
-      "application/x-extension-html" = ["zen.desktop"];
-      "application/x-extension-shtml" = ["zen.desktop"];
-      "application/xhtml+xml" = ["zen.desktop"];
-      "application/x-extension-xhtml" = ["zen.desktop"];
-      "application/x-extension-xht" = ["zen.desktop"];
+      "x-scheme-handler/http" = ["vivaldi-stable.desktop"];
+      "x-scheme-handler/https" = ["vivaldi-stable.desktop"];
+      "x-scheme-handler/chrome" = ["vivaldi-stable.desktop"];
+      "text/html" = ["vivaldi-stable.desktop"];
+      "application/x-extension-htm" = ["vivaldi-stable.desktop"];
+      "application/x-extension-html" = ["vivaldi-stable.desktop"];
+      "application/x-extension-shtml" = ["vivaldi-stable.desktop"];
+      "application/xhtml+xml" = ["vivaldi-stable.desktop"];
+      "application/x-extension-xhtml" = ["vivaldi-stable.desktop"];
+      "application/x-extension-xht" = ["vivaldi-stable.desktop"];
     };
   };
 }
