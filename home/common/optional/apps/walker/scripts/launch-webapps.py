ba61cb8f28bf1cb459d4a57d5ba6d80a6ebcdf72
Dileep Kishore <k.dileep1994@gmail.com>
2025-06-26 23:59:54 -0400

    feat(walker): add plugin to launch webapps

---
+++ home/common/optional/apps/walker/scripts/launch-webapps.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+LIST = [
+    "https://perplexity.ai",
+    "https://gemini.google.com",
+    "https://github.com/dileep-kishore",
+]
+
+
+def create_string(item: str) -> str:
+    label = f"label={item.removeprefix('https://')}"
+    exec = f'exec=vivaldi --app=\\"{item}\\"'
+    cmd = f"{label};{exec};matching=1;"
+    return cmd
+
+
+def main():
+    for item in LIST:
+        print(create_string(item))
+
+
+if __name__ == "__main__":
+    main()
