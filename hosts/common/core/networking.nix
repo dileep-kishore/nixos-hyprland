eb1f17e7b50ecbdef1f66b5b8a06da8d3959974e
Dileep Kishore <k.dileep1994@gmail.com>
2025-07-13 17:22:53 -0400

    feat(networking): update to latest version of hosts file

--- hosts/common/core/networking.nix
+++ hosts/common/core/networking.nix
@@ -8,10 +8,10 @@
     dhcpcd.enable = false;
     nameservers = ["208.67.222.123" "208.67.220.123"];
     extraHosts = let
-      hostsPath = "https://raw.githubusercontent.com/StevenBlack/hosts/1a8e75f1cb6cd3b26ff028df83121f3d7b67b04c/alternates/porn/hosts";
+      hostsPath = "https://raw.githubusercontent.com/StevenBlack/hosts/b89d4aad707876e47dcdf66f32fb26cfbcffc4e1/alternates/porn-only/hosts";
       hostsFile = builtins.fetchurl {
         url = hostsPath;
-        sha256 = "sha256:15vvv67piwmvgibjih8ifgs0q8z0pwsad5adyppijinq4fihwf4n";
+        sha256 = "sha256:05k080gb334dvfcxaqgr8pdw8zjhanc7fg44wg6fpjrqc01wri51";
       };
     in
       builtins.readFile "${hostsFile}";
